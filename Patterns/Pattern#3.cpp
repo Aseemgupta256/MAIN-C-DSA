cc50918a6ee8bca89a00b7f54e7f4ab0bb810fee
Aseem Gupta <500077426@stu.upes.ac.in>
2024-02-10 19:57:52 +0530

    Numbered Left Triangle

--- Patterns/Pattern#3.cpp
+++ Patterns/Pattern#3.cpp
@@ -1,4 +1,4 @@
-/*Program to create left triangle
+/*Program to create inverted left triangle
     *****
     ****
     ***
---
+++ Patterns/Pattern#4.cpp
@@ -0,0 +1,24 @@
+/*Program to create inverted left triangle
+    1
+    1 2
+    1 2 3
+    1 2 3 4
+    1 2 3 4 5 */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i =5;
+while(i>=1)
+{
+    int j=1;
+    do
+    {
+        cout<<j;
+        j++;
+    } while (j<=6-i);
+    cout<<endl;
+    i--;
+}
+    return 0;
+}
