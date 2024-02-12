6b11c683fd0d11ec4484319d9a7ca93125de2f09
Aseem Gupta <500077426@stu.upes.ac.in>
2024-02-13 00:15:46 +0530

    Full Diamond Edges Inverted

---
+++ Patterns/Pattern#12.cpp
@@ -0,0 +1,55 @@
+/*Program to create Half Diamond with Spaces Inverted
+     * * * * *
+      * * * *
+       * * *
+        * *
+         *
+         *
+        * *
+       * * *
+      * * * *
+     * * * * * */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n = 5;
+    int i =1;
+while(i<=2*n)
+{
+    if(i<=5)
+    {
+    int j=1;
+    while (j<=i-1)
+    {
+        cout<<" ";
+        j++;
+    }
+    int d = 1;
+    while(d<=n-i+1)
+    {
+        cout<<"*";
+        cout<<" ";
+        d++;
+    }
+  }
+  else{
+       int j=1;
+    while (j<=2*n-i)
+    {
+        cout<<" ";
+        j++;
+    }
+    int d = 1;
+    while(d<=i-n)
+    {
+        cout<<"*";
+        cout<<" ";
+        d++;
+    }
+  }
+    cout<<endl;
+    i++; 
+}
+    return 0;
+}
