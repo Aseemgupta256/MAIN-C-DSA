7c70d765f31f379b90a55e2155b3abcacdee4e53
Aseem Gupta <500077426@stu.upes.ac.in>
2024-02-12 23:11:23 +0530

    Half Diamond Inverted

--- Patterns/Pattern#9.cpp
+++ Patterns/Pattern#9.cpp
@@ -1,22 +1,22 @@
-/*Program to create Half Diamond
-        *
-       ***
-      *****
+/*Program to create Half Diamond Invert
+    *********
      *******
-    ********* */
+      *****
+       ***
+        * */
 #include<iostream>
 using namespace std;
 int main()
 {
     int n = 5;
-    int i =1;
-while(i<=n)
+    int i =5;
+while(i>0)
 {
-    int j=n-i;
-    while (j>0)
+    int j=1;
+    while (j<=n-i)
     {
         cout<<" ";
-        j--;
+        j++;
     }
     int k=2*i-1;
     int d = 1;
@@ -26,7 +26,7 @@ while(i<=n)
         d++;
     }
     cout<<endl;
-    i++;
+    i--;
 }
     return 0;
 }
